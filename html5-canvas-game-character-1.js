c12ef31b59ab7668db2fc53801146f3af419efb3
svkamboj@gmail.com <svkamboj@gmail.com>
2014-05-19 02:23:21 -0700

    Adding coconut animation, not complete

--- html5-canvas-game-character-1.js
+++ html5-canvas-game-character-1.js
@@ -41,6 +41,8 @@ var livesCount = 3;
 var jumpCount = 0;
 var cocoArray = [];
 var numCoconuts = 6;
+var timer = new Timer();
+var clockTick = null;
 
 function updateFPS() {
 	
@@ -77,6 +79,7 @@ function prepareCanvas(canvasDiv, canvasWidth, canvasHeight)
 	loadImage("leftArm-jump");
 	loadImage("legs-jump");
 	loadImage("Coconut");
+	loadImage("cocosprite");
 
 }
 
@@ -194,12 +197,19 @@ function redraw() {
     context.fillText(":" + livesCount, 900, 40);
     ++numFramesDrawn;
 
+    //drawEllipse(charX, charY, 20, 20);
+
+    clockTick = timer.tick();
     this.fillCocoArray(canvas);
     this.drawCoconuts(canvas);
     this.updateArray();
 
-    score += 1;
+    context.beginPath();
+    context.moveTo(0, canvas.height - 100);
+    context.lineTo(canvas.width, canvas.height - 100);
+    context.stroke();
 
+    score += 1;
    
 }
 
@@ -297,12 +307,15 @@ function Coconut(x, y) {
     this.y = y;
     this.removeFromWorld = false;
     this.fallSpeed = Math.floor(Math.random() * 5) + 2;
+    this.animation = new Animation(images["cocosprite"], 0, 0, 93, 65, 0.02, 6, true, false);
+    
+
 }
 
 /** Function that increments y coordinate of coconut */
 Coconut.prototype.fall = function () {
 
-    if (this.y < canvas.height) {
+    if (this.y < (canvas.height - (100 /*+ images["Coconut"].height*/))) {
         this.y += this.fallSpeed;
 
     } else {
@@ -313,5 +326,83 @@ Coconut.prototype.fall = function () {
 
 /**Draws a coconut*/
 Coconut.prototype.draw = function () {
-    context.drawImage(images["Coconut"], this.x, this.y);
+    if (this.y > canvas.height - 100) {   //+ images["Coconut"].height)
+        //context.drawImage(images["cocobreak"], this.x, this.y);
+        this.animation.drawFrame(clockTick, context, this.x, this.y);
+
+    } else {
+        context.drawImage(images["Coconut"], this.x, this.y);
+        
+    }
+    
+}
+
+function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
+    this.spriteSheet = spriteSheet;
+    this.startX = startX;
+    this.startY = startY;
+    this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.reverse = reverse;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+    var scaleBy = scaleBy || 1;
+    this.elapsedTime += tick;
+    if (this.loop) {
+        if (this.isDone()) {
+            this.elapsedTime = 0;
+        }
+    } else if (this.isDone()) {
+        return;
+    }
+    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
+    var vindex = 0;
+    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
+        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
+        vindex++;
+    }
+    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
+        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
+        vindex++;
+    }
+
+    var locX = x;
+    var locY = y;
+    var offset = vindex === 0 ? this.startX : 0;
+    ctx.drawImage(this.spriteSheet,
+                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+                  this.frameWidth, this.frameHeight,
+                  locX, locY,
+                  this.frameWidth * scaleBy,
+                  this.frameHeight * scaleBy);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+function Timer() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.wallLastTimestamp = 0;
+}
+
+Timer.prototype.tick = function () {
+    var wallCurrent = Date.now();
+    var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+    this.wallLastTimestamp = wallCurrent;
+
+    var gameDelta = Math.min(wallDelta, this.maxStep);
+    this.gameTime += gameDelta;
+    return gameDelta;
 }
