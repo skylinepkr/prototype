2d39126f2fc6771946de50772bc2c8e9a524db26
ats3216 <stringhamalex@gmail.com>
2014-05-29 13:34:56 -0700

    New barrel enemy
    added a rolling barrel enemy, and stopped the character from constantly
    moving backwards (got in the way when trying to jump over them).

--- html5-canvas-game-character-1.js
+++ html5-canvas-game-character-1.js
@@ -21,6 +21,7 @@ var numResourcesLoaded = 0;
 var fps = 30;
 var charX = 245;
 var charY = 473; //185
+var charCurrX;
 var breathInc = 0.1;
 var breathDir = 1;
 var breathAmt = 0;
@@ -48,8 +49,10 @@ var score = 0;
 var livesCount = 5;
 var jumpCount = 0;
 var cocoArray = [];
+var barrelArray = [];
 var highscorelist = [0,0,0,0,0];
 var numCoconuts = 3;
+var numBarrels = 2;
 var menuval = 0;
 var state = {
   MainMenu : {value: 0, name: "MainMenu"}, 
@@ -128,6 +131,7 @@ function prepareCanvas(canvasDiv, canvasWidth, canvasHeight)
 	loadImage("legs-jump");
 	loadImage("Coconut");
 	loadImage("cocoSprite");
+	loadImage("barrelSprites");
 
 }
 
@@ -355,11 +359,6 @@ function redraw() {
 
         }
 
-        //gradually go left as time passes
-        if (!(x < 0)) {
-            x = x - 3;
-            charX = x;
-        }
 
         //draw shadow
         if (jumping) {
@@ -414,8 +413,11 @@ function redraw() {
         ++numFramesDrawn;
 
         this.fillCocoArray(canvas);
+        this.fillBarrelArray(canvas);
         this.drawCoconuts(canvas);
+        this.drawBarrels(canvas);
         this.updateArray();
+        this.removeBarrel();
 
         score += 1;
 
@@ -562,6 +564,17 @@ function drawCoconuts(canvas) {
 
 }
 
+/*
+ * Draws the barrels into the array
+*/
+function drawBarrels(canvas) {
+    for (var i = 0; i < barrelArray.length; i++) {
+        var barrel = barrelArray[i];
+        barrel.draw(canvas);
+        barrel.roll();
+    }
+}
+
 /* 
 * Removes coconuts that have fallen on the ground
 */
@@ -576,6 +589,17 @@ function updateArray() {
     }*/
 }
 
+/* 
+* Removes barrels rolled off screen
+*/
+function removeBarrel() {
+    for (var i = barrelArray.length - 1; i >= 0; --i) {
+        if (barrelArray[i].removeFromWorld) {
+            barrelArray.splice(i, 1);
+        }
+    }
+}
+
 /*
 * Add coconuts to the coconut array 
 */
@@ -587,6 +611,17 @@ function fillCocoArray(canvas) {
     }
     //full = true;
 }
+
+/*
+* Adds Barrels to barrel array
+*/
+function fillBarrelArray(canvas) {
+
+    for (var i = 0; i < numBarrels - barrelArray.length; i++) {
+        barrelArray.push(new Barrel(1000, 453));
+    }
+}
+
 
 /* 
  * Contructs a coconut with given x,y coordinates
@@ -602,12 +637,37 @@ function Coconut(x, y) {
 }
 
 /* 
+ * Contructs a Barrel with given x,y coordinates
+*/
+function Barrel(x, y) {
+    this.x = x;
+    this.y = y;
+    this.removeFromWorld = false;
+    this.rollSpeed = Math.floor(Math.random() * 5) + 4;
+    this.animation = new Animation(images["barrelSprites"], 0, 0, 80, 68, 0.1, 4, true, false);
+}
+
+/* 
  * Function that increments y coordinate of coconut
  */
 Coconut.prototype.fall = function () {
 
     if (this.y < (canvas.height - (100 /*+ images["Coconut"].height*/))) {
         this.y += this.fallSpeed;
+
+    } else {
+        this.removeFromWorld = true;
+    }
+
+}
+
+/* 
+ * Function that increments x coordinate of Barrel
+ */
+Barrel.prototype.roll = function () {
+
+    if (this.x > 0) {
+        this.x -= this.rollSpeed;
 
     } else {
         this.removeFromWorld = true;
@@ -628,6 +688,13 @@ Coconut.prototype.draw = function () {
         
     }
     
+}
+
+/*
+Draws a Barrel (currently a coconut until we get the graphics for a barrel)
+ */
+Barrel.prototype.draw = function () {
+    this.animation.drawFrame(clockTick, context, this.x, this.y);
 }
 
 function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
